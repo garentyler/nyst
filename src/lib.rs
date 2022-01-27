a1a24642535bab69192d8236d0d139dd6db77c50
Garen Tyler <garentyler@gmail.com>
2022-01-27 09:11:55 -0700

    v0.2.0 - No longer require mutable reference to self, improve standard library

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,21 @@
 /// Pre-built parsers for convenience.
 pub mod parser;
 
-use std::sync::{Arc, RwLock};
-
 /// Generic parser trait.
 /// Only `Output` and `parse_unprotected` need to be defined to apply the trait.
 pub trait Parser {
   type Output;
   /// Parse `data` given `offset`.
   /// May leave `offset` in an incorrect state on failure.
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output>;
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output>;
   /// A wrapper around `parse_unprotected` that resets `offset` to what it was before parsing on failure.
-  fn parse(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
+  fn parse(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
     let offset_before = *offset;
     let parse_result = self.parse_unprotected(data, offset);
     if parse_result.is_err() {
       *offset = offset_before;
     }
     parse_result
-  }
-  /// Helper method to contain `self` in a `Box<Self>`.
-  fn into_boxed(self: Self) -> Box<Self> where Self: Sized {
-    Box::new(self)
-  }
-  /// Helper method to contain `Self` in a `Box`, made generic to `Box<dyn Parser>`.
-  fn into_boxed_generic(self: Self) -> Box<dyn Parser<Output = Self::Output>> where Self: Sized + 'static {
-    Box::new(self) as Box<dyn Parser<Output = Self::Output>>
-  }
-  /// Helper method to contain `Self` in an `Arc<RwLock<Self>>`.
-  fn into_arc_rwlock(self: Self) -> Arc<RwLock<Self>> where Self: Sized {
-    Arc::new(RwLock::new(self))
-  }
-  /// Helper method to contain `Self` in an `Arc<RwLock<Self>>`, made generic to `Arc<RwLock<dyn Parser>>`.
-  fn into_arc_rwlock_generic(self: Self) -> Arc<RwLock<dyn Parser<Output = Self::Output>>> where Self: Sized + 'static {
-    Arc::new(RwLock::new(self)) as Arc<RwLock<dyn Parser<Output = Self::Output>>>
   }
 }
 
@@ -45,6 +27,5 @@ pub type ParseResult<T> = Result<T, ParseError>;
 pub enum ParseError {
     InvalidData,
     NotEnoughData,
-    LockedSubparser,
     Io(std::io::Error),
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,33 +1,24 @@
 use crate::*;
-use std::sync::{Arc, RwLock};
+
+use std::cmp::PartialEq;
 
 /// Parses and then ignores the result, only advancing `offset`.
-pub struct IgnoreParser<A> {
-  pub parser: Arc<RwLock<dyn Parser<Output = A>>>,
+#[derive(Copy, Clone)]
+pub struct IgnoreParser<'a, A> {
+  pub parser: &'a dyn Parser<Output = A>,
 }
-impl<A> IgnoreParser<A> {
-  pub fn new(parser: &Arc<RwLock<dyn Parser<Output = A>>>) -> Self {
+impl<'a, A> IgnoreParser<'a, A> {
+  pub fn new(parser: &'a dyn Parser<Output = A>) -> Self {
     IgnoreParser {
-      parser: Arc::clone(parser),
+      parser,
     }
   }
 }
-impl<A> Clone for IgnoreParser<A> {
-  fn clone(&self) -> Self {
-    IgnoreParser {
-      parser: Arc::clone(&self.parser),
-    }
-  }
-}
-impl<A> Parser for IgnoreParser<A> {
+impl<'a, A> Parser for IgnoreParser<'a, A> {
   type Output = ();
 
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
-    let parse_result = match self.parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    }.parse(data, offset);
-    match parse_result {
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
+    match self.parser.parse(data, offset) {
       Ok(_result) => Ok(()),
       Err(err) => Err(err),
     }
@@ -35,135 +26,92 @@ impl<A> Parser for IgnoreParser<A> {
 }
 
 /// Parses the left parser and then the right parser, and then joins the result in a tuple.
-pub struct AndParser<A, B> {
-  pub left_parser: Arc<RwLock<dyn Parser<Output = A>>>,
-  pub right_parser: Arc<RwLock<dyn Parser<Output = B>>>,
+#[derive(Copy, Clone)]
+pub struct AndParser<'a, 'b, A, B> {
+  pub left_parser: &'a dyn Parser<Output = A>,
+  pub right_parser: &'b dyn Parser<Output = B>,
 }
-impl<A, B> AndParser<A, B> {
-  pub fn new(left_parser: &Arc<RwLock<dyn Parser<Output = A>>>, right_parser: &Arc<RwLock<dyn Parser<Output = B>>>) -> Self {
+impl<'a, 'b, A, B> AndParser<'a, 'b, A, B> {
+  pub fn new(left_parser: &'a dyn Parser<Output = A>, right_parser: &'b dyn Parser<Output = B>) -> Self {
     AndParser {
-      left_parser: Arc::clone(left_parser),
-      right_parser: Arc::clone(right_parser),
+      left_parser,
+      right_parser,
     }
   }
 }
-impl<A, B> Clone for AndParser<A, B> {
-  fn clone(&self) -> Self {
-    AndParser {
-      left_parser: Arc::clone(&self.left_parser),
-      right_parser: Arc::clone(&self.right_parser),
-    }
-  }
-}
-impl<A, B> Parser for AndParser<A, B> {
+impl<'a, 'b, A, B> Parser for AndParser<'a, 'b, A, B> {
   type Output = (A, B);
 
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
-    let left_result = match self.left_parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    }.parse(data, offset)?;
-    let right_result = match self.right_parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    }.parse(data, offset)?;
-    // let left_result = self.left_parser.parse(data, offset)?;
-    // let right_result = self.right_parser.parse(data, offset)?;
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
+    let left_result = self.left_parser.parse(data, offset)?;
+    let right_result = self.right_parser.parse(data, offset)?;
     Ok((left_result, right_result))
   }
 }
 
 /// A container for either the left result or the right result.
+#[derive(Copy, Clone, PartialEq)]
 pub enum OrParserSuccess<A, B> {
   Left(A),
   Right(B),
 }
 /// Parses the left parser and then the right parser, returning the first successful result.
-pub struct OrParser<A, B> {
-  pub left_parser: Arc<RwLock<dyn Parser<Output = A>>>,
-  pub right_parser: Arc<RwLock<dyn Parser<Output = B>>>,
+#[derive(Copy, Clone)]
+pub struct OrParser<'a, 'b, A, B> {
+  pub left_parser: &'a dyn Parser<Output = A>,
+  pub right_parser: &'b dyn Parser<Output = B>,
 }
-impl<A, B> OrParser<A, B> {
-  pub fn new(left_parser: &Arc<RwLock<dyn Parser<Output = A>>>, right_parser: &Arc<RwLock<dyn Parser<Output = B>>>) -> Self {
+impl<'a, 'b, A, B> OrParser<'a, 'b, A, B> {
+  pub fn new(left_parser: &'a dyn Parser<Output = A>, right_parser: &'b dyn Parser<Output = B>) -> Self {
     OrParser {
-      left_parser: Arc::clone(left_parser),
-      right_parser: Arc::clone(right_parser),
+      left_parser,
+      right_parser,
     }
   }
 }
-impl<A, B> Clone for OrParser<A, B> {
-  fn clone(&self) -> Self {
-    OrParser {
-      left_parser: Arc::clone(&self.left_parser),
-      right_parser: Arc::clone(&self.right_parser),
-    }
-  }
-}
-impl<A, B> Parser for OrParser<A, B> {
+impl<'a, 'b, A, B> Parser for OrParser<'a, 'b, A, B> {
   type Output = OrParserSuccess<A, B>;
 
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<OrParserSuccess<A, B>> {
-    let left_result = match self.left_parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    }.parse(data, offset);
-    match left_result {
-      Ok(result) => return ParseResult::Ok(OrParserSuccess::Left(result)),
-      Err(_) => {},
-    }
-    let right_result = match self.right_parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    }.parse(data, offset);
-    match right_result {
-      Ok(result) => Ok(OrParserSuccess::Right(result)),
-      Err(e) => Err(e),
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<OrParserSuccess<A, B>> {
+    if let Ok(result) = self.left_parser.parse(data, offset) {
+      ParseResult::Ok(OrParserSuccess::Left(result))
+    } else {
+      match self.right_parser.parse(data, offset) {
+        Ok(result) => Ok(OrParserSuccess::Right(result)),
+        Err(e) => Err(e),
+      }
     }
   }
 }
 
 /// Repeatedly applies a parser over a given range.
-pub struct RangeParser<A> {
-  pub parser: Arc<RwLock<dyn Parser<Output = A>>>,
+#[derive(Clone)]
+pub struct RangeParser<'a, A> {
+  pub parser: &'a dyn Parser<Output = A>,
   pub range: std::ops::Range<usize>,
 }
-impl<A> RangeParser<A> {
-  pub fn range(parser: &Arc<RwLock<dyn Parser<Output = A>>>, range: std::ops::Range<usize>) -> Self {
+impl<'a, A> RangeParser<'a, A> {
+  pub fn range(parser: &'a dyn Parser<Output = A>, range: std::ops::Range<usize>) -> Self {
     RangeParser {
-      parser: Arc::clone(parser),
+      parser,
       range,
     }
   }
-  pub fn repetitions(parser: &Arc<RwLock<dyn Parser<Output = A>>>, repetitions: usize) -> Self {
+  pub fn repetitions(parser: &'a dyn Parser<Output = A>, repetitions: usize) -> Self {
     RangeParser {
-      parser: Arc::clone(parser),
-      range: std::ops::Range {
-        start: repetitions,
-        end: repetitions,
-      },
+      parser,
+      range: repetitions..repetitions,
     }
   }
 }
-impl<A> Clone for RangeParser<A> {
-  fn clone(&self) -> Self {
-    RangeParser {
-      parser: Arc::clone(&self.parser),
-      range: self.range.clone(),
-    }
-  }
-}
-impl<A> Parser for RangeParser<A> {
+impl<'a, A> Parser for RangeParser<'a, A> {
   type Output = Vec<A>;
 
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
     let mut out = vec![];
-    let mut parser = match self.parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    };
     // Parse the minimum amount required.
     for _ in 0..self.range.start {
-      out.push(parser.parse(data, offset)?);
+      out.push(self.parser.parse(data, offset)?);
     }
     // Parse the optional ones.
     let mut index = out.len() - 1;
@@ -172,7 +120,7 @@ impl<A> Parser for RangeParser<A> {
       if index >= self.range.end {
         break;
       }
-      if let Ok(result) = parser.parse(data, offset) {
+      if let Ok(result) = self.parser.parse(data, offset) {
         out.push(result);
       }
     }
@@ -181,35 +129,24 @@ impl<A> Parser for RangeParser<A> {
 }
 
 /// Parses without advancing `offset`, returning `None` on error.
-pub struct PeekParser<A> {
-  pub parser: Arc<RwLock<dyn Parser<Output = A>>>,
+#[derive(Copy, Clone)]
+pub struct PeekParser<'a, A> {
+  pub parser: &'a dyn Parser<Output = A>,
 }
-impl<A> PeekParser<A> {
-  pub fn new(parser: &Arc<RwLock<dyn Parser<Output = A>>>) -> Self {
+impl<'a, A> PeekParser<'a, A> {
+  pub fn new(parser: &'a dyn Parser<Output = A>) -> Self {
     PeekParser {
-      parser: Arc::clone(parser),
+      parser,
     }
   }
 }
-impl<A> Clone for PeekParser<A> {
-  fn clone(&self) -> Self {
-    PeekParser {
-      parser: Arc::clone(&self.parser),
-    }
-  }
-}
-impl<A> Parser for PeekParser<A> {
+impl<'a, A> Parser for PeekParser<'a, A> {
   type Output = Option<(A, usize)>;
 
-  fn parse_unprotected(self: &mut Self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
+  fn parse_unprotected(&self, data: &[u8], offset: &mut usize) -> ParseResult<Self::Output> {
     let offset_before = *offset;
 
-    let mut parser = match self.parser.try_write() {
-      Ok(p) => p,
-      Err(_) => return Err(ParseError::LockedSubparser),
-    };
-
-    match parser.parse(data, offset) {
+    match self.parser.parse(data, offset) {
       Ok(result) => {
         let offset_delta = *offset - offset_before;
         *offset = offset_before;
